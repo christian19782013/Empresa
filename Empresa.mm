d29a398a9d0f9a032740b667d51c06e7c980b58c
christian19782013 <christian19782013@gmail.com>
2015-08-31 16:27:40 -0500

    INICIAL
    INICIAL

---
+++ Empresa.mm
@@ -0,0 +1,148 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1436223726521" ID="ID_1043249958" MODIFIED="1436234639255" TEXT="EMPRESA">
+<font NAME="SansSerif" SIZE="20"/>
+<node CREATED="1436234420083" HGAP="80" ID="ID_1228454057" MODIFIED="1436320437251" POSITION="right" TEXT="Misi&#xf3;n" VSHIFT="62">
+<font NAME="SansSerif" SIZE="28"/>
+<node CREATED="1436234448845" HGAP="19" ID="ID_655405781" MODIFIED="1436320323702" VSHIFT="-68">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Apoyar a la juventud ecuatoriana a que tome la mejor decisi&#243;n para su carrera profesional de acuerdo a par&#225;metros internacionales de valoraciones de aptitud
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1436234442652" HGAP="91" ID="ID_673294997" MODIFIED="1436320441197" POSITION="right" TEXT="Visi&#xf3;n" VSHIFT="97">
+<font NAME="SansSerif" SIZE="28"/>
+<node CREATED="1436234522082" HGAP="22" ID="ID_1048997718" MODIFIED="1436320654574" VSHIFT="99">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Para el 2018 ser la empresa que lidere el mercado ecuatoriano en&#160;el apoyo a las mejores deciciones de carreras universitarias de los aspirantes ecuatorianos.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1436320657645" ID="ID_406042809" MODIFIED="1436320682027">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      La proyecci&#243;n es trabajar y logra tener un staff de colaboradores de 100 personas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1436320268890" HGAP="82" ID="ID_147134520" MODIFIED="1436321049048" POSITION="right" TEXT="Metodolog&#xed;a de trabajo" VSHIFT="32">
+<font NAME="SansSerif" SIZE="28"/>
+<icon BUILTIN="bookmark"/>
+<node CREATED="1436320453351" HGAP="48" ID="ID_1728226128" MODIFIED="1436321053703" TEXT="Nivel Educativo" VSHIFT="42">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1436320480081" HGAP="95" ID="ID_1510124604" MODIFIED="1436321162269" VSHIFT="-87">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Nos vamos a basar originalmente en el libro de Jim Barret
+    </p>
+    <p>
+      &quot;Career, aptitude an selection test&quot;.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Se debe comunicar periodicamente para ver como entienden los
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1436321061250" HGAP="89" ID="ID_251491005" MODIFIED="1436321158569" VSHIFT="48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Se debe buscar una metodolog&#237;a para actualizarnos constantemente con las t&#233;cnicas que permitan a la organizaci&#243;n tener un mejor ROI .
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1436320469382" HGAP="56" ID="ID_680125582" MODIFIED="1436321057383" TEXT="Aplicativo" VSHIFT="32">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1436320357498" HGAP="42" ID="ID_1931673771" MODIFIED="1436320423006" VSHIFT="69">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Se&#160;tiene que desarrollar un software que permita a los aspirantes de ingreso a la universidad a tomar una mejor descici&#243;n
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1436320693839" HGAP="39" ID="ID_1292268309" MODIFIED="1436320905898" VSHIFT="73">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Se va a trabajar tratando de documentar todo lo que se hace y pasar el conocimiento entre los mucho s colaboradores, la meta en este punto es genrenciar el proyecto y ser los negociadores del mismo.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      En cualquier caso se debe proyectar para la persona adecuada maneje cada &#225;rea de la organizaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1436320969154" HGAP="40" ID="ID_1572392864" MODIFIED="1436321034233" VSHIFT="39">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      La metodolog&#237;a de trabajo permitir&#225;&#160;que todos losprocesos se manejen en la base de dato y posteriormente se realice el aplicativo en el front-ent
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ Software.mm
@@ -0,0 +1,14 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1438995088803" ID="ID_373699643" MODIFIED="1438995119480" TEXT="Aplicativo">
+<node CREATED="1438995123370" ID="ID_1276206157" MODIFIED="1438995207676" POSITION="right" TEXT="Motivaci&#xf3;n">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1438995208758" ID="ID_1447432425" MODIFIED="1438995229976" POSITION="right" TEXT="Aptitud">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1438995231097" ID="ID_990893554" MODIFIED="1438995240617" POSITION="right" TEXT="Personalidad">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</map>
